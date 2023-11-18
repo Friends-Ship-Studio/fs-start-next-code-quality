27859f723ef0fed6ecd4d60e971ac4bc82407b87
Kyle Kilat <kkilat@users.noreply.github.com>
2023-11-18 12:45:36 -0800

    chore: add and configure lint-staged

---
+++ .lintstagedrc.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+const buildTypeScriptCommand = () => `tsc --noEmit`
+
+const buildFormatCommand = (filenames) =>
+  `prettier --write ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' ')}`
+
+const buildEslintCommand = (filenames) =>
+  `next lint --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`
+
+module.exports = {
+  '*.{ts,tsx}': [
+    buildTypeScriptCommand,
+    buildFormatCommand,
+    buildEslintCommand,
+  ],
+  '*.{js,jsx,mjs,cjs}': [buildFormatCommand, buildEslintCommand],
+  '*.{json,html,css,md,yml,yaml}': [buildFormatCommand],
+}
