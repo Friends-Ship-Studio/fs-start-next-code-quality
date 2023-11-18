7ff9d85537f39bcf649d36659849e81a575e5495
Kyle Kilat <kkilat@users.noreply.github.com>
2023-11-18 10:03:39 -0800

    chore: load .gitignore as base ESLint ignore patterns

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
+const fs = require("fs");
+
 module.exports = {
+  ignorePatterns: [
+    // Load .gitignore content as base ESLint ignore patterns
+    ...fs.readFileSync(".gitignore", "utf8")
+        .split("\n")
+        .filter(line => line && !line.startsWith("#")),
+    // Include any additional ignore patterns here
+  ],
   extends: ["eslint:recommended", "next/core-web-vitals"]
 };
